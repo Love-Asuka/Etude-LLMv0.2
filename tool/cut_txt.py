40ba8351dc8be09d6d65bad928c170f7d89d96c0
Love Asuka <154034002+Love-Asuka@users.noreply.github.com>
2025-08-24 19:30:11 +0800

    Update cut_txt.py

--- tool/cut_txt.py
+++ tool/cut_txt.py
@@ -3,31 +3,31 @@ import shutil
 
 def split_and_move_files(big_text_dir='big_text', text_dir='text', chunk_size=2048*2048):
 
-    # 确保目标文件夹存在
+
     if not os.path.exists(text_dir):
         os.makedirs(text_dir)
     
-    # 遍历big_text文件夹中的所有文件
+
     for filename in os.listdir(big_text_dir):
         file_path = os.path.join(big_text_dir, filename)
         
-        # 如果是文件而不是目录
+
         if os.path.isfile(file_path):
             try:
                 with open(file_path, 'rb') as f:
                     part_num = 1
                     while True:
-                        # 读取块数据
+
                         chunk = f.read(chunk_size)
                         if not chunk:
                             break
                         
-                        # 创建分割后的文件名
+
                         base_name, ext = os.path.splitext(filename)
                         new_filename = f"{base_name}_part{part_num}{ext}"
                         new_file_path = os.path.join(text_dir, new_filename)
                         
-                        # 写入分割后的文件
+
                         with open(new_file_path, 'wb') as chunk_file:
                             chunk_file.write(chunk)
                         
